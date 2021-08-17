1fc065d6a2af78029710e292fe6e1a4325017d5f
Sang Nguyen <sangminhnguyen1999@gmail.com>
2021-08-18 01:34:52 +0700

    initial commit

--- data.py
+++ data.py
@@ -1,9 +1,28 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
 class Dataset:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.df = pd.read_csv(data)
 
-    def load_dataset(self):
-        pass
+    def _removeHTMLRegex(text) -> str:
+        soup = BeautifulSoup(text)
+        return soup.get_text().lower().replace(r'[^a-zA-Z0-9]',' ')
+    
+    def _stemming(text) -> str:
+        sw = set(stopwords.words('english'))
+        snow = SnowballStemmer('english')
+        text=' '.join([snow.stem(word) for word in text.split() if word not in sw])
+        return text
+    
+    def _cleanData(self, data):
+        self.df.review = self.df.review.apply(_removeHTMLRegex).apply(_stemming)
+        self.df.sentiment.replace({'negative':0, 'positive':1}, inplace=True)
 
-    def build_dataset(self):
-        pass
+### TODO: Train test split + tokenizer 
