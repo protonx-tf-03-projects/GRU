a1cd414b4df1465ffe76d10ada021fec17b63703
Duong Tran <ttduongtran@gmail.com>
2021-08-22 00:14:13 +0700

    ref args model and loss funs

--- train.py
+++ train.py
@@ -14,13 +14,17 @@ if __name__ == "__main__":
     parser.add_argument(
         "--data_folder", default='{}/data'.format(home_dir), type=str)
     parser.add_argument(
-        "--model-folder", default='{}/model/mlp/'.format(home_dir), type=str)
+        "--model-folder", default='{}/model/'.format(home_dir), type=str)
     
+    parser.add_argument("--model", default='gru', type=str)
     parser.add_argument("--units", default=128, type=int)
     parser.add_argument("--embedding-size", default=1000, type=int)
     parser.add_argument("--vocab-size", default=1000, type=int)
     parser.add_argument("--input-length", default=1000, type=int)
     parser.add_argument("--learning-rate", default=0.001, type=float)
+    parser.add_argument("--optimizers", default='rmsprop', type=str)
+    parser.add_argument("--loss-function",
+                        default='binary_crossentropy', type=str)
 
     parser.add_argument("--batch-size", default=64, type=int)
     parser.add_argument("--epochs", default=1000, type=int)
@@ -52,27 +56,45 @@ if __name__ == "__main__":
     val_ds = None  # <-- fixme
 
     # Initializing model
-    gru_rnn = GRU_RNN(args.units, args.embedding_size, args.vocab_size, args.input_length)
+    if args.model == 'lstm':
+      # LSTM model
+      # model = LSTM()
+      pass
+    elif args.model == 'tanh':
+      # Tanh model
+      # model = Tanh()
+      pass 
+    else:
+      model = GRU_RNN(args.units, args.embedding_size,
+                        args.vocab_size, args.input_length)
 
     # Set up loss function
-    loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=False, label_smoothing=0,axis=-1, reduction="auto", name="binary_crossentropy")
+    if args.loss_function == 'binary_crossentropy':
+      loss_object = tf.keras.losses.BinaryCrossentropy(
+          from_logits=False, label_smoothing=0, axis=-1, reduction="auto", name="binary_crossentropy")
+    else:
+      # Adam or whatever
+      pass
 
     # Optimizer Definition
-    rmsprop = tf.keras.optimizers.RMSprop(
-        learning_rate=args.learning_rate, rho=0.9, momentum=0.0, epsilon=1e-07, centered=False, name='RMSprop')
+    if args.loss_function == 'rmsprop':
+      optimizers = tf.keras.optimizers.RMSprop(
+          learning_rate=args.learning_rate, rho=0.9, momentum=0.0, epsilon=1e-07, centered=False, name=args.optimizers)
+    else: 
+      pass
 
     # Compile optimizer and loss function into model
-    gru_rnn.compile(optimizer=rmsprop, loss=loss_object, metrics=['acc'])
+    model.compile(optimizer=optimizers, loss=loss_object, metrics=['acc'])
 
     # Callbacks
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
 
     # Training model
-    gru_rnn.fit(train_ds, epochs=args.epochs, batch_size=args.batch_size,
+    model.fit(train_ds, epochs=args.epochs, batch_size=args.batch_size,
                 validation_data=val_ds, callbacks=[early_stopping])
 
     # Saving model
-    gru_rnn.save(args.model_folder)
+    model.save(args.model_folder)
 
     # Do Prediction
 
