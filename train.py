1b1cc87e0b98032353f54873047ea24f3ee0a188
Duong Tran <ttduongtran@gmail.com>
2021-09-03 13:41:34 +0700

    Add: builder data and training model

--- train.py
+++ train.py
@@ -1,31 +1,42 @@
 import os
 from argparse import ArgumentParser
 import tensorflow as tf
-from tensorflow.python.keras.optimizer_v2 import rmsprop
 from model.gru_rnn import GRU_RNN
+from data_builder import Dataset
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 if __name__ == "__main__":
+    tf.keras.backend.clear_session()
     parser = ArgumentParser()
     parser.add_argument("--logdir", default="logs")
     home_dir = os.getcwd()
 
-    # FIXME
     # Arguments users used when running command lines
     parser.add_argument(
-        "--data_folder", default='{}/data'.format(home_dir), type=str)
+        "--mode-folder", default='{}/output/'.format(home_dir), type=str)
+
+    parser.add_argument("--data-path", default='data/mock/IMDB_Dataset_mini.csv', type=str)
+    parser.add_argument("--data-name", default='review', type=str)
+    parser.add_argument("--label-name", default='sentiment', type=str)
     parser.add_argument(
-        "--model-folder", default='{}/model/'.format(home_dir), type=str)
-    
+        "--data-classes", default={'negative': 0, 'positive': 1}, type=set)
+    parser.add_argument("--num-class", default=2, type=int)
+
     parser.add_argument("--model", default='gru', type=str)
     parser.add_argument("--units", default=128, type=int)
-    parser.add_argument("--embedding-size", default=1000, type=int)
-    parser.add_argument("--vocab-size", default=1000, type=int)
-    parser.add_argument("--input-length", default=1000, type=int)
-    parser.add_argument("--learning-rate", default=0.001, type=float)
+    parser.add_argument("--embedding-size", default=128, type=int)
+    parser.add_argument("--vocab-size", default=128, type=int)
+    parser.add_argument("--max-length", default=512, type=int)
+    
+    
+    parser.add_argument("--learning-rate", default=0.008, type=float)
     parser.add_argument("--optimizers", default='rmsprop', type=str)
-
-    parser.add_argument("--batch-size", default=64, type=int)
-    parser.add_argument("--epochs", default=1000, type=int)
+    parser.add_argument("--test-size", default=0.2, type=float)
+    parser.add_argument("--batch-size", default=16, type=int)
+    parser.add_argument("--buffer-size", default=128, type=int)
+    parser.add_argument("--epochs", default=10, type=int)
 
     args = parser.parse_args()
 
@@ -36,22 +47,28 @@ if __name__ == "__main__":
     print('Github: ${accout}')
     print('Email: ${email}')
     print('---------------------------------------------------------------------')
-    print('Training ${name} model with hyper-params:') # FIXME
+    print('Training ${args.model} model with hyper-params:')
     print('===========================')
     
     # print arguments
     for i, arg in enumerate(vars(args)):
-        print('{}.{}: {}'.format(i, arg, vars(args)[arg]))
+        print('{}. {}: {}'.format(i, arg, vars(args)[arg]))
     print('===========================')
     
-    # FIXME
 
     # Prepair dataset
-    data_folder = args.data_folder
+    
+    dataset = Dataset(args.data_path, args.vocab_size, data_classes=args.data_classes)
+    
+    train_ds, val_ds = dataset.build_dataset(
+        args.max_length, args.test_size, args.buffer_size, args.batch_size, args.data_name, args.label_name)
 
-    # Split data
-    train_ds = None # <-- fixme
-    val_ds = None  # <-- fixme
+    sentences_tokenizer = dataset.sentences_tokenizer
+    sentences_tokenizer_size = len(sentences_tokenizer.word_counts) + 1
+    print("sentences_tokenizer_size: ", sentences_tokenizer_size)
+    
+    # Initializing variables
+    input_length = args.max_length
 
     # Initializing model
     if args.model == 'lstm':
@@ -64,31 +81,33 @@ if __name__ == "__main__":
       pass 
     else:
       model = GRU_RNN(args.units, args.embedding_size,
-                        args.vocab_size, args.input_length)
+                      sentences_tokenizer_size, input_length, num_class=args.num_class)
 
     # Set up loss function
-    loss_object = tf.keras.losses.CategoricalCrossentropy(
-        from_logits=False, label_smoothing=0, axis=-1, reduction="auto", name="categorical_crossentropy")
+    losses = tf.keras.losses.CategoricalCrossentropy(
+        name="categorical_crossentropy")
         
     # Optimizer Definition
-    if args.loss_function == 'rmsprop':
+    if args.optimizers == 'rmsprop':
       optimizers = tf.keras.optimizers.RMSprop(
           learning_rate=args.learning_rate, name='rmsprop')
     else: 
       pass
 
     # Compile optimizer and loss function into model
-    model.compile(optimizer=optimizers, loss=loss_object, metrics=['acc'])
+    metrics = ['accuracy']
+    model.compile(optimizer=optimizers,
+                  loss=losses, metrics=metrics)
 
     # Callbacks
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
 
     # Training model
-    model.fit(train_ds, epochs=args.epochs, batch_size=args.batch_size,
-                validation_data=val_ds, callbacks=[early_stopping])
+    history = model.fit(train_ds, epochs=args.epochs,
+                        batch_size=args.batch_size, validation_data=val_ds, verbose=1)
 
     # Saving model
-    model.save(args.model_folder)
+    # model.save_weights(args.model_folder)
 
     # Do Prediction
 
