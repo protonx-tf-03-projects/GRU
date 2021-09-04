a2bb7d607f260b555409baa5b57006945c553cf3
Duong Tran <ttduongtran@gmail.com>
2021-09-04 17:04:11 +0700

    add checkpoint callbacks

--- train.py
+++ train.py
@@ -15,9 +15,11 @@ if __name__ == "__main__":
 
     # Arguments users used when running command lines
     parser.add_argument(
-        "--mode-folder", default='{}/output/'.format(home_dir), type=str)
+        "--model-folder", default='{}/tmp/model/'.format(home_dir), type=str)
+    parser.add_argument(
+        "--checkpoint-folder", default='{}/tmp/checkpoints/'.format(home_dir), type=str)
 
-    parser.add_argument("--data-path", default='data/IMDB_Dataset.csv', type=str)
+    parser.add_argument("--data-path", default='data/IMDB_Dataset_mini.csv', type=str)
     parser.add_argument("--data-name", default='review', type=str)
     parser.add_argument("--label-name", default='sentiment', type=str)
     parser.add_argument(
@@ -25,18 +27,17 @@ if __name__ == "__main__":
     parser.add_argument("--num-class", default=2, type=int)
 
     parser.add_argument("--model", default='gru', type=str)
-    parser.add_argument("--units", default=128, type=int)
+    parser.add_argument("--units", default=256, type=int)
     parser.add_argument("--embedding-size", default=128, type=int)
-    parser.add_argument("--vocab-size", default=128, type=int)
+    parser.add_argument("--vocab-size", default=512, type=int)
     parser.add_argument("--max-length", default=512, type=int)
     
-    
-    parser.add_argument("--learning-rate", default=0.008, type=float)
+    parser.add_argument("--learning-rate", default=0.005, type=float)
     parser.add_argument("--optimizer", default='rmsprop', type=str)
     parser.add_argument("--test-size", default=0.2, type=float)
-    parser.add_argument("--batch-size", default=16, type=int)
+    parser.add_argument("--batch-size", default=32, type=int)
     parser.add_argument("--buffer-size", default=128, type=int)
-    parser.add_argument("--epochs", default=10, type=int)
+    parser.add_argument("--epochs", default=12, type=int)
 
     args = parser.parse_args()
 
@@ -102,12 +103,21 @@ if __name__ == "__main__":
     # Callbacks
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
 
+    # include the epoch in the file name
+    # checkpoint_path = 'tmp/checkpoints/cp-{epoch:04d}.ckpt/'
+    # checkpoint_dir = os.path.dirname(checkpoint_path)
+    checkpoint_path = '{args.checkpoint_folder}/{args.model}-cp-{epoch:04d}.ckpt/'
+
+    # Save weights, every 4-epochs.
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        checkpoint_path, verbose=1, save_weights_only=True, period=4)
+
     # Training model
     history = model.fit(train_ds, epochs=args.epochs,
-                        batch_size=args.batch_size, validation_data=val_ds, verbose=1)
+                        batch_size=args.batch_size, validation_data=val_ds, verbose=1, callbacks=[checkpoint])
 
     # Saving model
-    # model.save_weights(args.model_folder)
+    # model.save(args.model_folder, save_format='tf')
 
     # Do Prediction
 
