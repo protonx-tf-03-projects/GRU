2dfdf0267290032a9f65077027592c79aafc1ae3
Duong Tran <ttduongtran@gmail.com>
2021-09-07 13:40:39 +0700

    change activation

--- model/gru_rnn.py
+++ model/gru_rnn.py
@@ -5,38 +5,52 @@ from tensorflow.python.keras.layers.embeddings import Embedding
 class GRU(tf.keras.layers.Layer):
   '''
     Arguments:
-    units: 
-    input_shape
+      units (int): hidden dimension 
+      inp_shape (int): Embedding dimension 
+    Output:
+      h_t (Tensor): 
+        Current hidden state
+        shape=(None, units) 
   '''
 
-  def __init__(self, units, _input_shape):
+  def __init__(self, units, inp_shape):
     super(GRU, self).__init__()
     self.units = units
-    self._input_shape = _input_shape
-    self.W = self.add_weight("W", shape=(3, self.units, self._input_shape))
-    self.U = self.add_weight("U", shape=(3, self.units, self.units))
+    self.inp_shape = inp_shape
+    self.W = self.add_weight("W", shape=(3, self.units, self.inp_shape))
+    self.U = self.add_weight("U", shape=(3, self.inp_shape, self.units))
+    self.b = self.add_weight("b", shape=(self.units, 1))
 
   def call(self, pre_h, x):
 
     # Update gate: Decide how much the unit updates its activation, or content
     z_t = tf.nn.sigmoid(
-        tf.matmul(x, tf.transpose(self.W[0])) + tf.matmul(pre_h, tf.transpose(self.U[0])))
+        tf.matmul(x, tf.transpose(self.W[0])) + tf.matmul(pre_h, tf.transpose(self.U[0])) + self.b[0])
 
     # Reset gate: Forget the previously state
     r_t = tf.nn.sigmoid(
-        tf.matmul(x, tf.transpose(self.W[1])) + tf.matmul(pre_h, tf.transpose(self.U[1])))
+        tf.matmul(x, tf.transpose(self.W[1])) + tf.matmul(pre_h, tf.transpose(self.U[1])) + self.b[1])
 
     # Current memory content
     h_proposal = tf.nn.tanh(
-        tf.matmul(x, tf.transpose(self.W[2])) + tf.matmul(tf.multiply(r_t, pre_h), tf.transpose(self.U[2])))
+        tf.matmul(x, tf.transpose(self.W[2])) + tf.matmul(tf.multiply(r_t, pre_h), tf.transpose(self.U[2])) + self.b[2])
 
     # Current hidden state
     h_t = tf.multiply((1 - z_t), pre_h) + tf.multiply(z_t, h_proposal)
-
+    
     return h_t
 
 # Define GRU model
 class GRU_RNN(tf.keras.Model):
+  '''
+    Arguments
+      units (int):
+      embedding_size (int):
+      vocab_size (int):
+      input_length (int):
+      num_class (int):
+    Output
+  '''
   def __init__(self, units, embedding_size, vocab_size, input_length, num_class):
     super(GRU_RNN, self).__init__()
     self.input_length = input_length
@@ -55,13 +69,15 @@ class GRU_RNN(tf.keras.Model):
 
     # Pass each hidden state through Rnn basic
     self.classification_layer = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(32, input_shape=(units,), activation="swish"),
+        tf.keras.layers.Dense(32, input_shape=(units,), activation="elu"),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(num_class, activation='softmax')
     ])
 
   def call(self, sentence):
-
+    '''
+      sentence
+    '''
     batch_size = tf.shape(sentence)[0]
 
     # Initial hidden_state
--- train.py
+++ train.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--checkpoint-folder", default='{}/tmp/checkpoints/'.format(home_dir), type=str)
 
-    parser.add_argument("--data-path", default='data/IMDB_Dataset_mini.csv', type=str)
+    parser.add_argument("--data-path", default='data/IMDB_Dataset.csv', type=str)
     parser.add_argument("--data-name", default='review', type=str)
     parser.add_argument("--label-name", default='sentiment', type=str)
     parser.add_argument(
@@ -27,12 +27,12 @@ if __name__ == "__main__":
     parser.add_argument("--num-class", default=2, type=int)
 
     parser.add_argument("--model", default='gru', type=str)
-    parser.add_argument("--units", default=256, type=int)
+    parser.add_argument("--units", default=128, type=int)
     parser.add_argument("--embedding-size", default=128, type=int)
-    parser.add_argument("--vocab-size", default=512, type=int)
-    parser.add_argument("--max-length", default=512, type=int)
+    parser.add_argument("--vocab-size", default=10000, type=int)
+    parser.add_argument("--max-length", default=256, type=int)
     
-    parser.add_argument("--learning-rate", default=0.005, type=float)
+    parser.add_argument("--learning-rate", default=0.0008, type=float)
     parser.add_argument("--optimizer", default='rmsprop', type=str)
     parser.add_argument("--test-size", default=0.2, type=float)
     parser.add_argument("--batch-size", default=32, type=int)
@@ -41,14 +41,13 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # FIXME
     # Project Description
-
-    print('---------------------Welcome to ${name}-------------------')
-    print('Github: ${accout}')
-    print('Email: ${email}')
+    print(' ')
+    print('---------------------Welcome to GRU Team | TF03 | ProtonX-------------------')
+    print('Github: joeeislovely | anhdungpro97 | ttduongtran')
+    print('Email: nguyenminh.sangatpa@gmail.com | anhdung1951997@gmail.com | ttduongtran@gmail.com')
     print('---------------------------------------------------------------------')
-    print('Training ${args.model} model with hyper-params:')
+    print(f'Training {args.model.upper()} model with hyper-params:')
     print('===========================')
     
     # print arguments
@@ -56,9 +55,7 @@ if __name__ == "__main__":
       print('{}. {}: {}'.format(i, arg, vars(args)[arg]))
     print('===========================')
     
-
     # Prepair dataset
-    
     dataset = Dataset(args.data_path, args.vocab_size, data_classes=args.data_classes)
     
     train_ds, val_ds = dataset.build_dataset(
@@ -82,7 +79,8 @@ if __name__ == "__main__":
     else:
       model = GRU_RNN(args.units, args.embedding_size,
                       sentences_tokenizer_size, input_length, num_class=args.num_class)
-
+    
+    
     # Set up loss function
     losses = tf.keras.losses.CategoricalCrossentropy(
         name="categorical_crossentropy")
@@ -96,28 +94,33 @@ if __name__ == "__main__":
           learning_rate=args.learning_rate, name='adam')
 
     # Compile optimizer and loss function into model
-    metrics = ['accuracy']
+    metrics = ['accuracy', 'mse']
     model.compile(optimizer=optimizer,
                   loss=losses, metrics=metrics)
+    
+    # model.summary()
 
-    # Callbacks
+    # Callbacks: Early stopping
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
 
+    # Callbacks: checkpoint training
     # include the epoch in the file name
-    # checkpoint_path = 'tmp/checkpoints/cp-{epoch:04d}.ckpt/'
-    # checkpoint_dir = os.path.dirname(checkpoint_path)
-    checkpoint_path = '{args.checkpoint_folder}/{args.model}-cp-{epoch:04d}.ckpt/'
+    checkpoint_path = "tmp/checkpoints/cp-{epoch:04d}.ckpt/"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
 
     # Save weights, every 4-epochs.
     checkpoint = tf.keras.callbacks.ModelCheckpoint(
-        checkpoint_path, verbose=1, save_weights_only=True, period=4)
+        checkpoint_path, verbose=1, save_weights_only=True, save_freq=5)
 
     # Training model
-    history = model.fit(train_ds, epochs=args.epochs,
-                        batch_size=args.batch_size, validation_data=val_ds, verbose=1, callbacks=[checkpoint])
+    model.fit(train_ds, epochs=args.epochs,
+                        batch_size=args.batch_size, 
+                        validation_data=val_ds,
+                        verbose=1, 
+                        callbacks=[checkpoint])
 
     # Saving model
-    # model.save(args.model_folder, save_format='tf')
+    model.save(f"{args.model_folder}/{args.model}.h5py")
 
     # Do Prediction
 
